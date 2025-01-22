9c888114aa78df4cee1968b9d64cc87796ee53f7
Zergus325 <alex.usenko.97@gmail.com>
2025-01-22 15:11:02 +0200

    add routes

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import initMongoConnection from './db/initMongoConnection.js';
+import contactsRouter from './routes/contacts.js';
 
 dotenv.config();
 
@@ -14,19 +15,17 @@ const setupServer = async () => {
   app.use(cors());
   app.use(pino());
 
+  app.use('/contacts', contactsRouter);
+
   app.get('/', (req, res) => {
     res.send('Welcome to the server!');
-  });
-
-  app.get('/contacts', (req, res) => {
-    res.json({ message: 'Contact page' });
   });
 
   app.use((_, res) => {
     res.status(404).json({ message: 'Not found' });
   });
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 4000;
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
