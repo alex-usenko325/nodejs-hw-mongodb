071318a5493457ee41283e93b0052b25df1b2808
Zergus325 <alex.usenko.97@gmail.com>
2025-01-27 18:44:31 +0200

    fix contacts service

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import Contact from '../models/contact.js';
+import Contact from '../module/contacts.js';
 
 const getAll = async () => Contact.find({});
 const getById = async (id) => Contact.findById(id);
