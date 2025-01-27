30c8fa924f840dfea398cdbbe89264e59d5fce1d
Zergus325 <alex.usenko.97@gmail.com>
2025-01-27 18:54:04 +0200

    fix export contact service

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,16 +1,8 @@
 import Contact from '../module/contacts.js';
 
-const getAll = async () => Contact.find({});
-const getById = async (id) => Contact.findById(id);
-const create = async (data) => Contact.create(data);
-const update = async (id, data) =>
+export const getAll = async () => Contact.find({});
+export const getById = async (id) => Contact.findById(id);
+export const create = async (data) => Contact.create(data);
+export const update = async (id, data) =>
   Contact.findByIdAndUpdate(id, data, { new: true });
-const remove = async (id) => Contact.findByIdAndDelete(id);
-
-export default {
-  getAll,
-  getById,
-  create,
-  update,
-  remove,
-};
+export const remove = async (id) => Contact.findByIdAndDelete(id);
