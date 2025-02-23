fce6bd55841ce22fcae5872a3bc2bac951808375
Zergus325 <alex.usenko.97@gmail.com>
2025-02-23 12:05:50 +0200

    add upload photo with create contacts

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -62,7 +62,18 @@ export const createContact = async (req, res, next) => {
     }
 
     console.log('Creating contact with userId:', userId);
-    const newContact = await create({ ...req.body, userId });
+    const photo = req.file;
+    let photoUrl;
+
+    if (photo) {
+      if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+        photoUrl = await saveFileToCloudinary(photo);
+      } else {
+        photoUrl = await saveFileToUploadDir(photo);
+      }
+    }
+
+    const newContact = await create({ ...req.body, userId, photo: photoUrl });
 
     res
       .status(201)
