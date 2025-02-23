e1d5be07ae61ed369013fff6b293fc28c5735081
Zergus325 <alex.usenko.97@gmail.com>
2025-02-23 11:46:45 +0200

    fix err getEnvVar

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,14 +1,14 @@
 import cloudinary from 'cloudinary';
 import fs from 'node:fs/promises';
 
-import { getEvnVar } from './getEnvVar.js';
+import { getEnvVar } from './getEnvVar.js';
 import { CLOUDINARY } from '../constants/index.js';
 
 cloudinary.v2.config({
   secure: true,
-  cloud_name: getEvnVar(CLOUDINARY.CLOUD_NAME),
-  api_key: getEvnVar(CLOUDINARY.API_KEY),
-  api_secret: getEvnVar(CLOUDINARY.API_SECRET),
+  cloud_name: getEnvVar(CLOUDINARY.CLOUD_NAME),
+  api_key: getEnvVar(CLOUDINARY.API_KEY),
+  api_secret: getEnvVar(CLOUDINARY.API_SECRET),
 });
 
 export const saveFileToCloudinary = async (file) => {
